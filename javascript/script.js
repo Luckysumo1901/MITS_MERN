b6e02a5bb8c0652a29b50e8982538f14cbb3673e
Tejaswani <ramiganitejaswani@gmail.com>
2026-02-27 16:04:56 +0530

    Performed java sripts

---
+++ javascript/script.js
@@ -0,0 +1,76 @@
+//console.log("Hello World!!");
+//var can be reinitialize and redeclare
+/*var a; //Global scope
+a=10;
+var a;
+a=20;
+//reinitialiae
+let b=10; //Block scope
+b=20;
+//can't reinitialize and redeclare
+const c=30; //Block scope
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+//Datatypes
+/*
+var num=10;
+var str="Sumedha"; //no char only strings('' "" ``)
+var bool=true;
+var un;
+var n=null;
+var sym=Symbol('li');
+var big=12348327498632n;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof un);
+console.log(typeof n);
+console.log(typeof sym);
+console.log(typeof big);
+
+var arr=[1,2,3,4,5]
+console.log(arr);
+console.log(typeof arr);
+
+var Obj={
+    name:"Sumedha",
+    dept:"DS"
+}
+console.log(Obj);
+console.log(typeof Obj);*/
+
+/*var a=10;
+var b=20;
+console.log(a+b);
+console.log(a-b);
+
+var a=10;
+var b="2";
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+var a=10;
+var b=20;
+console.log(a=b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a<b);
+console.log(a>b);
+console.log(a<=b);
+console.log(a>=b);*/
+
+/*var a=true;
+var b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+console.log(!b);*/
+
