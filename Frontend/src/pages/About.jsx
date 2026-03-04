cca53f0f3f3041e35b3b5ae00aeacbe30b8b0e8c
sumedha <sumedhavennapusa@gmail.com>
2026-03-04 09:14:56 +0530

    updates sucess

---
+++ Frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import Parent from "../components/Parent";
+
+
+const About = () => {
+    const element = <div> Hello World!!</div>
+  return (
+    <div>
+      <p>{element}</p>
+      <Parent/>
+    </div>
+  )
+}
+
+export default About
---
+++ Frontend/src/pages/Services.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Services =()=> {
+        return(
+            <>
+                <div>
+                        <h1>Services</h1>
+                        <p>Class Component</p>
+                </div>      
+                <p>Fragment Tag</p>      
+            </>
+        )
+}
+
+export default Services
