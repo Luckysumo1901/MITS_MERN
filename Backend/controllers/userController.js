854365cead316ece4de033e766a6232acac93ebb
sumedha <sumedhavennapusa@gmail.com>
2026-03-10 14:26:49 +0530

    Created Todo router and controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,25 +1,65 @@
-exports.getUsers = (req,res)=>{
-    res.send("Get All Users");
-}
+const User=require('../models/user')
 
-exports.getUserById = (req,res)=>{
-    res.send("Get User By Id");
-}
+exports.getUsers= async(req,res)=>{
+    try{
+        const users=await User.find();
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).send(err);
+    }
+    
+};
+exports.getUserById=async (req,res)=>{
+    try{
+        const user=await User.findById(req.params.id);
+        if(!user){
+            return res.status(404).json({message:"USer Not Find"});
+        }
+        res.status(200).json(user);
+    }catch{
+        res.status(500).send(err);
+    }
+};
+exports.createUser= async(req,res)=>{
+    try{
+        const{name,email,password}=req.body;
+        const user=await User.findOne({email});
+        if(user){
+            return res.status(401).json({message:"User Already Exist"});
+        }
+        const data=await User.create({
+            name,
+            email,
+            password,
+        });
+        res.status(200).json(data);
+    }catch(err){
+        res.status(500).send(err);
+    }
+};
+exports.updateUser=async(req,res)=>{
+    try{
+        const user=await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!user){
+            return res.status(404).json({message:"User Not Exist"});
+        }
+        res.status(200).json(user);
 
-exports.createUser = (req,res)=>{
-    res.json({
-        message: "User created successfully"
-    });
+    }catch(err){
+        res.status(500).send(err);
+    }
+   
 }
+exports.deleteUser=async(req,res)=>{
+    try{
+       const user= await User.findByIdAndDelete(req.params.id);
+       if(!user){
+         return res.status(404).json({message:"User Not Found"});
+        }
+        res.status(200).json({message:"User Deleted Successfully"})
 
-exports.updateUser = (req,res)=>{
-    res.json({
-        message: "User updated successfully"
-    });
-}
-
-exports.deleteUser = (req,res)=>{
-    res.json({
-        message: "User deleted successfully"
-    });
-}
+    }catch(err){
+        res.status(500).send(err);
+    }
+  
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const connectDB = require('./config/db')
-const dotenv = require('dotenv')
+const express =require('express');
+const connectDB=require('./config/db')
+const dotenv=require('dotenv')
 const app=express();
-const egRouters = require('./routers/egRouter')
-const userRouters = require('./routers/userRouter')
+const egRouters=require('./routers/egRouter')
+const userRouters=require('./routers/userRouter')
+const todoRouters=require('./routers/todoRouter')
+const cors=require('cors')
+
 dotenv.config();
-
 connectDB();
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json)
-app.use(egRouters)
-app.use('/api/user',userRouters)
+app.use(egRouters);
+app.use('/api/user',userRouters);
+app.use('/api/todo',todoRouters);
 
-
-const PORT = process.env.PORT;
-
+const PORT=process.env.PORT;
 app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
+    console.log(`Server is Running on http://localhost:${PORT}`)
+})  
